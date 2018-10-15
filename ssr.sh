1c605e2f632d8b7e68a2666e495f425ccf1b104f
Zhang <2358858416@qq.com>
2018-10-15 14:52:52 +0800

    ssr update

--- ssr.sh
+++ ssr.sh
@@ -2,3 +2,10 @@
 yum update -y
 yum install vim git -y
 git clone -b manyuser https://github.com/ToyoDAdoubi/shadowsocksr.git
+
+# 开启防火墙端口
+firewall-cmd --add-port=8888/tcp
+firewall-cmd --add-port=666/tcp
+
+#显示已经开启的端口
+firewall-cmd --zone=public --list-ports
