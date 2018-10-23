7b8d7b5374d4a69dae04eaf07057c1941692fbee
Zhang <504947985@qq.com>
2018-10-23 19:30:43 +0800

    add python3_install.sh

---
+++ python3_install.sh
@@ -0,0 +1,14 @@
+yum install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gcc make
+
+wget https://www.python.org/ftp/python/3.5.2/Python-3.5.2.tgz
+
+tar -zxvf Python-3.5.2.tgz
+
+cd Python-3.5.2
+
+./configure prefix=/usr/local/python3
+
+make && make install
+
+ln -s /usr/local/python3/bin/python3.5 /usr/bin/python3
+ln -s /usr/local/python3/bin/pip3 /usr/bin/pip3
