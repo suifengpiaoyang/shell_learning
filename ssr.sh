ae6529ee42def679dff86782c575953ba8b604ff
Zhang <504947985@qq.com>
2018-10-24 19:52:04 +0800

    add python3_install.sh

--- ssr.sh
+++ ssr.sh
@@ -4,8 +4,8 @@ yum install vim git -y
 git clone -b manyuser https://github.com/ToyoDAdoubi/shadowsocksr.git
 
 # 开启防火墙端口
-firewall-cmd --add-port=8888/tcp --permanent
-firewall-cmd --add-port=666/tcp --permanent
+firewall-cmd --add-port=8888/tcp
+firewall-cmd --add-port=666/tcp
 
 #显示已经开启的端口
 firewall-cmd --zone=public --list-ports
